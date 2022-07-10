dcbad68eac8cbd5e300ed9efc08cce6c2f13174b
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-07-10 22:25:44 +0900

    spin

--- components/Spin.tsx
+++ components/Spin.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Spin = () => (
   <div className="flex w-full justify-center">
-    <div className="my-20 h-20 w-20 animate-spin  rounded-full border-4 border-green-300 border-t-transparent border-r-transparent" />
+    <div className="my-4 h-10 w-10 animate-spin  rounded-full border-4 border-green-300 border-t-transparent border-r-transparent" />
   </div>
 );
